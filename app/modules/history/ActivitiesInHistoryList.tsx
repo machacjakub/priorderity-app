52d30fac845bdf66a43c610aba61f33cfbf8d759
jakubmachac <jakub.machac@beafuture.com>
2023-09-21 23:57:43 +0200

    server actions and useOptimistic introduced

--- app/modules/history/ActivitiesInHistoryList.tsx
+++ app/modules/history/ActivitiesInHistoryList.tsx
@@ -2,8 +2,8 @@ import {IDoneActivity} from "@/app/types";
 import {ActivityInHistory} from "@/app/modules/history/ActivityInHistory";
 
 
-export const ActivitiesInHistoryList = ( {activities}: { activities: IDoneActivity[] } ) => {
+export const ActivitiesInHistoryList = ( {activities, handleDelete}: { activities: IDoneActivity[], handleDelete: ( id: number ) => void } ) => {
 	return ( <div>
-		{activities.map( activity => <div key={activity.id}><ActivityInHistory activity={activity}/></div> )}
+		{activities.map( activity => <div key={activity.id}><ActivityInHistory activity={activity} handleDelete={handleDelete} /></div> )}
 	</div> );
 };
