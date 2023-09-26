8e2bcb5dca3a4dce4d45fb4020a1c1fdf782316d
jakubmachac <jakub.machac@beafuture.com>
2023-09-26 23:42:48 +0200

    useOptimistic hook for doneActivities

--- app/App.tsx
+++ app/App.tsx
@@ -8,11 +8,11 @@ import {ActivitiesToAdd} from "@/app/modules/attributes-stats/ActivitiesToAdd";
 import {DoneActivitiesHistory} from "@/app/modules/history/DoneActivitiesHistory";
 import {HealthBars} from "@/app/modules/health-bars/HealthBars";
 import {getHealthStats} from "@/app/modules/health-bars/utils";
-import { experimental_useOptimistic as useOptimistic } from 'react';
 import {ActivitiesToDo} from "@/app/modules/todo/ActivitiesToDo";
 import {ProfileNavigation} from "@/app/modules/profile/ProfileNavigation";
 import useBoolean from "@/app/utils/hooks/useBoolean";
 import {TodoForm} from "@/app/modules/todo/TodoForm";
+import useDoneActivities from "@/app/utils/hooks/useDoneActivities";
 
 interface IProps {
 	user: Nullable<User>;
@@ -22,13 +22,7 @@ interface IProps {
 export const App = ( {user, done, planned}: IProps ) => {
 	const profileNavIsDisplayed = useBoolean( false );
 	const todoFormDisplayed = useBoolean( false );
-	const [optimisticActivities, addOptimisticActivity] = useOptimistic<IDoneActivity[], string>(
-		done ?? [],
-		( state: IDoneActivity[], newActivityType: string ) => [
-			{ id: state[0].id + 1, type: newActivityType, created_at: new Date() },
-			...state,
-		]
-	);
+	const {doneActivities, addDoneActivity, deleteDoneActivity} = useDoneActivities( done ?? [] );
 	return (
 		<div className="w-full h-screen flex flex-col items-center">
 			<Navbar user={user} onProfileClick={profileNavIsDisplayed.setTrue} />
@@ -43,10 +37,10 @@ export const App = ( {user, done, planned}: IProps ) => {
 						<HealthBars healthStats={getHealthStats( done ?? [] )}/>
 					</div>
 					<div className="bg-background row-span-3 overflow-auto mt-16 top">
-						<DoneActivitiesHistory doneActivities={optimisticActivities}/>
+						<DoneActivitiesHistory doneActivities={doneActivities} handleDelete={deleteDoneActivity}/>
 					</div>
 					<div className="bg-background col-span-2 row-span-2 overflow-auto mt-16">
-						<ActivitiesToAdd onAdd={addOptimisticActivity}/>
+						<ActivitiesToAdd onAdd={addDoneActivity}/>
 					</div>
 				</div>
 			</div>
