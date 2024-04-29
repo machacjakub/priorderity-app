ec5f577481e712b09094ae7c943ce908197b1ce9
jakubmachac <jakub.machac@beafuture.com>
2024-04-29 09:54:19 +0200

    habits activities UI fix 2

--- app/settings/habits/ActivityToTrackAsHabit.tsx
+++ app/settings/habits/ActivityToTrackAsHabit.tsx
@@ -3,5 +3,5 @@ import { useContext } from "react";
 
 export const ActivityToTrackAsHabit = ( { activityType }: {activityType: string} ) => {
 	const activities = useContext( userDataContext )?.activities_stats;
-	return <div className='border-b-2 border-l-2 border-blue-400 p-1 px-2.5 mb-1 rounded-xl my-2 w-fit'>{activities?.find( a => a.type === activityType )?.label ?? activityType}</div>;
+	return <div className='border-b-2 border-l-2 border-blue-400 p-1 px-2.5 rounded-xl w-fit'>{activities?.find( a => a.type === activityType )?.label ?? activityType}</div>;
 };
